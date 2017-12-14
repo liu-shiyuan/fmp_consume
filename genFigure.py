b98b7c608401f03750555ec1a1888971a275f902
liu-shiyuan <ezio.leung@gmail.com>
2017-12-14 19:02:20 +0800

    add week figure

--- genFigure.py
+++ genFigure.py
@@ -10,7 +10,7 @@ mpl.rcParams['font.sans-serif'] = ['SimHei']
 
 
 def gen_last_7_day_chart():
-    results = get_last_7_day_consume()
+    results = get_records(last_7_days_sql())
     x_date = []
     y_cost = []
     for row in results:
@@ -30,25 +30,38 @@ def gen_last_7_day_chart():
 
 
 def gen_12_week_chart():
-    results = get_12_week_consume()
+    results = get_records(last_12_week_sql())
+    x_date = []
+    y_cost = []
+    for row in results:
+        x_date.append(row[1] + '-' + row[2])
+        y_cost.append(row[3])
+    fig1 = plt.figure(figsize=(20, 10))
+    plt.xticks(rotation=60)
+    plt.plot(x_date, y_cost, 'ro-')
+    plt.title('Performad每周消耗统计')
+    length = len(x_date)
+    index = 0
+    while index < length:
+        plt.text(x_date[index], y_cost[index], y_cost[index], rotation=75)
+        index = index + 1
+    plt.text(5, 1000, 'Baseline 1000', size=15)
+    plt.grid(True)
+    plt.axhline(1000, linewidth=2)
+    file_name = determine_default_file_path('Figure_2', 'png')
+    plt.savefig(file_name)
 
 
-def get_last_7_day_consume():
-    db = DBConnection().get_connection()
-    sql = last_7_days_sql()
-    cursor = db.cursor()
+def get_records(sql):
+    conn = DBConnection().get_connection()
+    cursor = conn.cursor()
     try:
         cursor.execute(sql)
         results = cursor.fetchall()
         return results
     except Exception as e:
         print(e)
-    db.close()
-
-
-def get_12_week_consume():
-    db = DBConnection().get_connection()
-    sql = last_12_week_sql()
+    conn.close()
 
 
 def last_7_days_sql():
@@ -67,8 +80,15 @@ def last_7_days_sql():
 
 
 def last_12_week_sql():
-
+    return """select DATE_FORMAT(str_to_date(report_date, '%Y/%m/%d'), '%Y%u') weeks
+    , min(DATE_FORMAT(str_to_date(report_date, '%Y/%m/%d'), '%m.%d')) from_date
+    , max(DATE_FORMAT(str_to_date(report_date, '%Y/%m/%d'), '%m.%d')) to_date
+    , round(sum(cost), 2) cost
+    from fb_online_report
+    group by weeks
+    order by weeks """
 
 
 if __name__ == '__main__':
     #gen_last_7_day_chart()
+    gen_12_week_chart()
